28c6f658a913276ccfb0deb7ec8ddba693f1640b
mateusz7812 <mateusz78122@gmail.com>
2022-04-19 16:33:50 +0200

    Add project files.

---
+++ GrpcFileClient/Program.cs
@@ -0,0 +1,122 @@
+﻿using Google.Protobuf;
+using Grpc.Net.Client;
+using GrpcFileService;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GrpcFileClient
+{
+    class Program
+    {
+        private const int ChunkSize = 1024 * 32;
+        static async Task Main(string [] args)
+        {
+            bool works = true;
+            while (works)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Pobieranie pliku");
+                Console.WriteLine("2. Wysyłanie pliku");
+                Console.WriteLine("0. Wyjście");
+                Console.Write("Wybór: ");
+                int choose; 
+                if(!Int32.TryParse(Console.ReadLine(), out choose))
+                {
+                    Console.WriteLine("Błąd odczytu wyboru");
+                    Console.ReadKey();
+                    continue;
+                }
+                switch (choose)
+                {
+                    case 0:
+                        works = false;
+                        break;
+                    case 1:
+                        DownloadFileMenu();
+                        break;
+                    case 2:
+                        await UploadFileMenuAsync();
+                        break;
+                    default:
+                        Console.WriteLine("Podano złą liczbę");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static void DownloadFileMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task UploadFileMenuAsync()
+        {
+            var CurrentDirectory = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Files");
+            string [] filePaths = Directory.GetFiles(CurrentDirectory, "*.txt", SearchOption.TopDirectoryOnly);
+            for (int i = 1; i <= filePaths.Length; i++)
+            {
+                string file = filePaths [i-1];
+                Console.WriteLine($"{i}. {file}");
+            }
+            Console.WriteLine("0. Anuluj");
+            Console.Write("Wybór: ");
+
+            int choose;
+            if (!Int32.TryParse(Console.ReadLine(), out choose))
+            {
+                Console.WriteLine("Błąd odczytu wyboru");
+                Console.ReadKey();
+                return;
+            }
+            await UploadFileAsync(filePaths [choose-1]);
+        }
+
+        private static async Task UploadFileAsync(string file_path)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Uploader.UploaderClient(channel);
+
+            Console.WriteLine("Nawiązywanie połączenia z serwerem");
+            var call = client.UploadFile();
+
+            Console.WriteLine("Wysyłanie metadanych pliku");
+            await call.RequestStream.WriteAsync(new UploadFileRequest
+            {
+                Metadata = new FileMetadata
+                {
+                    FileName = Path.GetFileName(file_path)
+                }
+            });
+
+            var buffer = new byte [ChunkSize];
+            await using var readStream = File.OpenRead(file_path);
+
+            while (true)
+            {
+                var count = await readStream.ReadAsync(buffer);
+                if (count == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Wysyłanie paczki danych o długości " + count);
+                await call.RequestStream.WriteAsync(new UploadFileRequest
+                {
+                    Data = UnsafeByteOperations.UnsafeWrap(buffer.AsMemory(0, count))
+                });
+            }
+
+            Console.WriteLine("Plik wysłany");
+            await call.RequestStream.CompleteAsync();
+
+            var response = await call;
+            Console.WriteLine("Id wysyłania: " + response.Id);
+
+            Console.WriteLine("Naciśnij dowolny klawisz aby powrócić do menu");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GrpcFileService/Services/UploadService.cs
@@ -0,0 +1,56 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Json.Net;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace GrpcFileService.Services
+{
+    public class UploadService : Uploader.UploaderBase
+    {
+
+        public override async Task<UploadFileResponse> UploadFile(IAsyncStreamReader<UploadFileRequest> requestStream, ServerCallContext context)
+        {
+            var FilesDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Files");
+            var uploadId = Path.GetRandomFileName();
+            var uploadPath = Path.Combine(FilesDirectory, uploadId);
+            Directory.CreateDirectory(uploadPath);
+            await DownloadBinary(requestStream, uploadPath);
+            ConvertBinaryToFile(FilesDirectory, uploadPath);
+            return new UploadFileResponse { Id = uploadId };
+        }
+
+        private static void ConvertBinaryToFile(string FilesDirectory, string uploadPath)
+        {
+            var metadata_json = File.ReadAllText(Path.Combine(uploadPath, "metadata.json"));
+            string file_name = JObject.Parse(metadata_json).GetValue("fileName").Value<string>();
+
+            byte [] fileBytes = File.ReadAllBytes(Path.Combine(uploadPath, "data.bin"));
+
+            File.WriteAllBytes(Path.Combine(FilesDirectory, file_name), fileBytes);
+        }
+
+        private static async Task DownloadBinary(IAsyncStreamReader<UploadFileRequest> requestStream, string uploadPath)
+        {
+            await using (var writeStream = File.Create(Path.Combine(uploadPath, "data.bin")))
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    if (message.Metadata != null)
+                    {
+                        await File.WriteAllTextAsync(Path.Combine(uploadPath, "metadata.json"), message.Metadata.ToString());
+                    }
+                    if (message.Data != null)
+                    {
+                        await writeStream.WriteAsync(message.Data.Memory);
+                    }
+                }
+            }
+        }
+    }
+}
